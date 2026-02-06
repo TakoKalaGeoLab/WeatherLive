0c880c3593e2bbd42d05f3588f87282749d76eb1
Marika Kalandia <takokala@yahoo.com>
2026-02-06 21:53:04 +0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherLive
+//
+//  Created by Marika Kalandia on 06.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
